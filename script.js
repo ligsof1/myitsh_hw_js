cfd5e3a931e36290157bd08a0be31885549113c1
ligsof <dina100009@gmail.com>
2025-02-06 17:27:03 +0300

    Update script.js
    hw6_js

--- script.js
+++ script.js
@@ -15,25 +15,25 @@ console.log(format);
 
 
 let str_3 = 'Я учу javascript!';
-let word1 = str_3.substr(2, 3); // "учу"
-let word2 = str_3.substr(6, 10); // "javascript"
+let word1 = str_3.substr(2, 3); 
+let word2 = str_3.substr(6, 10); 
 
-console.log(word1); // вывод: "учу"
-console.log(word2); // вывод: "javascript"
+console.log(word1); 
+console.log(word2); 
 
 let str_3_1 = 'Я учу javascript!';
-let word1_1 = str_3_1.substring(2, 5); // "учу"
-let word2_1 = str_3_1.substring(6, 16); // "javascript"
+let word1_1 = str_3_1.substring(2, 5); 
+let word2_1 = str_3_1.substring(6, 16); 
 
-console.log(word1_1); // вывод: "учу"
-console.log(word2_1); // вывод: "javascript"
+console.log(word1_1); 
+console.log(word2_1); 
 
 let str_3_2 = 'Я учу javascript!';
-let word1_2 = str_3_2.slice(2, 5); // "учу"
-let word2_2 = str_3_2.slice(6, 16); // "javascript"
+let word1_2 = str_3_2.slice(2, 5); 
+let word2_2 = str_3_2.slice(6, 16); 
 
-console.log(word1_2); // вывод: "учу"
-console.log(word2_2); // вывод: "javascript"
+console.log(word1_2); 
+console.log(word2_2); 
 
 
 let arr1 = [4, 2, 5, 19, 13, 0, 10];
@@ -114,21 +114,21 @@ console.log(getDateTime());
 
 let str7 = 'aa aba abba abbba abca abea';
 let matches = str7.match(/ab+a/g);
-console.log(matches); // вывод: ['aba', 'abba', 'abbba']
+console.log(matches); // ['aba', 'abba', 'abbba']
 
 
 
 
 function validatePhoneNumber(phoneNumber) {
-    const phoneRegex = /^\+\d{1,3} \d{1,4} \d{4,}$/;
-    return phoneRegex.test(phoneNumber);
+    const phoner = /^\+\d{1,3} \d{1,4} \d{4,}$/;
+    return phoner.test(phoneNumber);
 }
 
 console.log(validatePhoneNumber('+375 29 1234567')); // true
 
 
 
-function validateEmail(email) {
+function validEmail(email) {
     const emailRegex = /^[a-zA-Z0-9._%+-]{3,}@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,11}$/;
 
     if (!emailRegex.test(email)) {
@@ -150,26 +150,26 @@ function validateEmail(email) {
     return true;
 }
 
-console.log(validateEmail('example.email@example.com')); 
-console.log(validateEmail('e@ex.com')); 
-console.log(validateEmail('example@ex@ample.com')); 
-console.log(validateEmail('example@domain.russia')); 
-console.log(validateEmail('пример@пример.com')); 
+console.log(validEmail('example.email@example.by')); 
+console.log(validEmail('e@ex.by')); 
+console.log(validEmail('example@ex@ample.by')); 
+console.log(validEmail('example@domain.ru')); 
+console.log(validEmail('пример@пример.by')); 
 
 
 
 
-function extractUrlComponents(url) {
-    const urlPattern = /^(https?:\/\/[^\/?#]+)([^?#]*)(\?[^#]*)?(#.*)?$/;
-    const match = url.match(urlPattern);
+function UrlComponents(url) {
+    const urlPat = /^(https?:\/\/[^\/?#]+)([^?#]*)(\?[^#]*)?(#.*)?$/;
+    const match = url.match(urlPat);
 
     const domain = match[1] || '';
     const path = match[2] || '';
-    const queryParams = match[3] ? match[3].substring(1) : '';
+    const qParams = match[3] ? match[3].substring(1) : '';
     const fragment = match[4] ? match[4].substring(1) : '';
 
-    return [domain, path, queryParams, fragment];
+    return [domain, path, qParams, fragment];
 }
 
 const url = 'https://tech.onliner.by/2018/04/26/smart-do-200/?utm_source=main_tile&utm_medium=smartdo200#zag3';
-console.log(extractUrlComponents(url));
+console.log(UrlComponents(url));
